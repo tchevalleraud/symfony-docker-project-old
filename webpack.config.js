cd465d5b980bdac12078b18374517780da56c85b
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2021-08-14 16:19:43 +0200

    Update 20210814 #9

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/assets/')
+    .setPublicPath('/assets')
+    .enableVersioning()
+    .copyFiles({
+        from: './assets',
+        to: '[path][name].[ext]'
+    })
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+    .autoProvidejQuery();
+
+module.exports = Encore.getWebpackConfig();
